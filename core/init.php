652cf2157228333213691b405b0cea8b780b9b1d
joshua <jcoroneza@gmail.com>
2018-06-05 13:18:43 +0800

    Hays

--- core/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-	session_start();
-	require 'functions/functions.php';
-	if (isset($_SESSION['user_id'])){
-	   $user_data=user_data($_SESSION['user_id'],'user_id','fname','mname','lname','username','password');
-	}
-?>
